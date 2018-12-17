4558dad9eb180121505d9c25f3c3c849e90b98cb
Alain NOUTCHOMWO <alinolandry@gmail.com>
2018-12-17 23:07:40 +0100

    graph en event view integration

--- addons/openacademy/models.py
+++ addons/openacademy/models.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api 
 from odoo.exceptions import UserError, ValidationError
+from datetime import timedelta
 
 
 class Course(models.Model):
@@ -63,6 +64,10 @@ class Session(models.Model):
 	attendee_ids = fields.Many2many("res.partner", string="Attendees")
 
 	taken_seats = fields.Float(string="Taken seats", compute="_taken_seats")
+	end_date = fields.Date(string="End Date", store=True, compute="_get_end_date", inverse="_set_end_date")
+
+	attendees_count = fields.Integer(string="Attendees count", compute="_get_attentdees_count", store=True)
+
 	
 	# This decorator manage computer field
 	@api.depends("seats", "attendee_ids")
@@ -93,6 +98,36 @@ class Session(models.Model):
 					'message': "Increase seats or remove excess attendees"
 				},
 			}
+
+
+	@api.depends('start_date', 'duration')
+	def _get_end_date(self):
+		for r in self:
+			if not(r.start_date and r.duration):
+				r.end_date = r.start_date
+				continue
+
+			# Add duration to start_date, but : Munday+5=Saturday, so
+			#subtract one second to get on Friday intead
+			start = fields.Datetime.from_string(r.start_date)
+			duration = timedelta(days=r.duration, seconds=-1)
+			r.end_date = start + duration
+
+	def _set_end_date(self):
+		for r in self:
+			if not (r.start_date and r.end_date):
+				continue
+
+			start_date = fields.Datetime.from_string(r.start_date)
+			end_date = fields.Datetime.from_string(r.end_date)
+			r.duration = (end_date - start_date).days + 1	
+
+
+	@api.depends('attendee_ids')
+	def _get_attentdees_count(self):
+		for r in self:
+			r.attendees_count = len(r.attendee_ids)
+	
 
 	# Add contrain to control that instructor is not in attendee_ids list
 	@api.constrains('instructor_id', 'attendee_ids')
